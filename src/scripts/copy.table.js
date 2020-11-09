dbacf6994405c4a3f674c52c5fb6def70267da53
Facundo Merighi <merighifacundo@gmail.com>
2020-11-09 11:58:18 -0300

    Pushing and copying

--- src/scripts/copy.table.js
+++ src/scripts/copy.table.js
@@ -16,23 +16,56 @@ function clearGlobalSecondaryIndexes(GlobalSecondaryIndexes) {
   });
 }
 
-export default async function copyTable(fromTable, toTable, environmentFrom, environmentTo) {
-  const { Table } = await environmentFrom.db
+export async function copyDefinition(fromTable, toTable, environmentFrom, environmentTo) {
+	const { Table } = await environmentFrom.db
     .describeTable({ TableName: fromTable })
     .promise();
-
+	console.log(JSON.stringify(Table));
   const { KeySchema, AttributeDefinitions, GlobalSecondaryIndexes, ProvisionedThroughput } = Table;
 
-  const newTableDescription = {
+  let newTableDescription = {
     TableName: toTable,
     KeySchema,
     AttributeDefinitions,
-    GlobalSecondaryIndexes: clearGlobalSecondaryIndexes(GlobalSecondaryIndexes),
     ProvisionedThroughput: {
       ReadCapacityUnits: ProvisionedThroughput.ReadCapacityUnits,
       WriteCapacityUnits: ProvisionedThroughput.WriteCapacityUnits,
     },
-  };
-
+	};
+	const globalSecondaryIndex = clearGlobalSecondaryIndexes(GlobalSecondaryIndexes);
+	if (globalSecondaryIndex) {
+		newTableDescription.GlobalSecondaryIndexes = globalSecondaryIndex;
+	}
+	console.log(JSON.stringify(newTableDescription));
   return environmentTo.db.createTable(newTableDescription).promise();
+}
+
+export async function copyContent(fromTable, toTable, environmentFrom, environmentTo) {
+	let params = {
+		TableName: fromTable,
+	};
+	let items = null;
+	do {
+    items = await environmentFrom.db.scan(params).promise();
+		const request = {
+			RequestItems: {}
+		};
+		request.RequestItems[toTable] = items.Items.map(item => {
+			return {
+				PutRequest: {
+					Item: item
+				}
+			}
+		});
+		const result = await environmentTo.db.batchWriteItem(request).promise();
+		//this is to iterate all the pages.
+    params.ExclusiveStartKey = items.LastEvaluatedKey;
+  } while (typeof items.LastEvaluatedKey != 'undefined');
+}
+
+export default async function copyTable(fromTable, toTable, environmentFrom, environmentTo) {
+	const result = await copyDefinition(fromTable, toTable, environmentFrom, environmentTo);
+	console.log(JSON.stringify(result));
+	const resultSave = await copyContent(fromTable, toTable, environmentFrom, environmentTo)	
+	return resultSave;
 }
